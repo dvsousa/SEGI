cd04ef19a7d7003becddd79ea574ae3135e6d52f
David <david.email.br@gmail.com>
2017-12-15 00:11:54 -0200

    alterações

---
+++ SEGI/Models/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SEGI.Models
+{
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Mae { get; set; }
+    }
+}
